24a056a79bd2e1d6b5ba9404e267042a8992f522
joelcna <joelcna28@gmail.com>
2023-09-10 13:27:33 -0400

    Completed Tutorial
    Will add laser destroyer so the game doesn't get clogged up with a ton of lasers.
    
    Will also make enemy ships stop spawning when the game is over.
    
    Might make enemy ships gradually get faster
    
    Will add dodge mechanic

---
+++ FirstGame292/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    int score = 0;
+    bool isGameOver = false;
+    public static GameManager instance;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject gameOverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit") && isGameOver)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void increaseScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void gameOver()
+    {
+        isGameOver = true;
+        gameOverText.SetActive(true);
+    }
+}
